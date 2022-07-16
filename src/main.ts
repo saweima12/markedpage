e2ec3af73eaf274330d803d63f476852ff531f91
saweima12 <saweima12@gmail.com>
2022-07-16 19:07:17 +0800

    finish: markdown update vite hmr support

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 export { getPage, siteConfig, slugMap, pathMap, classifiedSet } from './index';
-export { markedpageVitePlugin, onContentUpdate, onSiteConfigUpdate } from './vite';
+export { markedpageVitePlugin, onContentUpdate } from './vite';
 
 export type { ClassifierHandle } from './classifier';
 export type { DirectoryClassifierResult } from './classifier/directory';
--- src/vite.ts
+++ src/vite.ts
@@ -1,21 +1,14 @@
 import path from 'path';
-import type { HmrContext, Plugin, InferCustomEventPayload } from 'vite';
+import type { HmrContext, Plugin } from 'vite';
 
 import { initClassifierMap } from './classifier';
-import { getConfig, loadConfig, loadSourcePages } from './source';
+import { getConfig, loadSourcePages } from './source';
 
 const CONTENT_UPDATE_EVENT = "markedpage:content-update"
-const SITECONFIG_UPDATE_EVENT = "markedpage:config-update"
 
 export const onContentUpdate = (callback: (payload: Record<string, any>) => void) => {
   if (import.meta.hot) {
     import.meta.hot.on(CONTENT_UPDATE_EVENT, callback)
-  }
-}
-
-export const onSiteConfigUpdate = (callback: (payload: Record<string, any>) => void) => {
-  if (import.meta.hot) {
-    import.meta.hot.on(SITECONFIG_UPDATE_EVENT, callback)
   }
 }
 
@@ -26,19 +19,12 @@ export const markedpageVitePlugin: Plugin = () => {
     configureServer(server) {
       // add watch path "${project_root}/docs"
       server.watcher.add(path.join(process.cwd(), "docs"));
-      server.watcher.add(path.join(process.cwd(), "/src/site.config.js"));
     },
     async handleHotUpdate(ctx: HmrContext) {
       const content_match = /\/docs\/(.+)\.md$/.exec(ctx.file);
-      const config_match = /\/site.config.js/.exec(ctx.file);
       // didn't match, use default behavior.
       if (content_match) {
         await onContentMatch(ctx, content_match[0], content_match[1]);
-        return [];
-      }
-
-      if (config_match) {
-        await onConfigMatch(ctx)
         return [];
       }
 
@@ -64,10 +50,4 @@ const onContentMatch = async(ctx: HmrContext, filePath:string, indexPath:string)
       indexPath: indexPath
     }
   });
-}
-
-const onConfigMatch = async (ctx: HmrContext) => {
-  // update client
-  console.info(`site.config changed: restarting vite server. - file: ${ctx.file}`);
-  ctx.server.restart()
 }
