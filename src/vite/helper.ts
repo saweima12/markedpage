fbb16ae9435a83c502c13058425ab48d08c1cdaf
saweima12 <saweima12@gmail.com>
2022-08-22 00:12:39 +0800

    set onContentUpdate internal

--- src/vite/helper.ts
+++ src/vite/helper.ts
@@ -1,5 +1,6 @@
 import { CONTENT_UPDATE_EVENT } from './common';
 
+/** @internal */
 export const onContentUpdate = (callback: (payload: Record<string, any>) => void) => {
   if (import.meta.hot) {
     import.meta.hot.on(CONTENT_UPDATE_EVENT, callback);
