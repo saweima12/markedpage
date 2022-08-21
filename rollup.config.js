8fbc049ef0c4d95acd07652eec65f581616c8b88
saweima12 <saweima12@gmail.com>
2022-08-22 00:07:15 +0800

    adjust rollup process.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
-import dts from 'rollup-plugin-dts';
 import sucrase from '@rollup/plugin-sucrase';
 import commonjs from '@rollup/plugin-commonjs';
+import flatDts from 'rollup-plugin-flat-dts';
 import excludeDependenciesFromBundle from 'rollup-plugin-exclude-dependencies-from-bundle';
 
 export default [
@@ -12,27 +12,24 @@ export default [
       sucrase({ transforms: ['typescript'] }),
       excludeDependenciesFromBundle({ dependencies: true })
     ],
-    input: ['src/index.ts'],
-    output: [{ file: "./dist/index.js", format: 'es', sourcemap: false }]
-  },
-  {
-    plugins: [
-      resolve({ preferBuiltins: true }),
-      commonjs(),
-      sucrase({ transforms: ['typescript'] }),
-      excludeDependenciesFromBundle({ dependencies: true })
-    ],
-    input: ['src/helper.ts'],
-    output: [{ file: "./dist/helper.js", format: 'es', sourcemap: false }]
-  },
-  {
-    plugins: [dts()],
-    input: ['src/index.ts'],
-    output: [{ file: 'dist/index.d.ts', format: 'es' }]
-  },
-  {
-    plugins: [dts()],
-    input: ['src/helper.ts'],
-    output: [{ file: 'dist/helper.d.ts', format: 'es' }]
+    input: {
+      index: 'src/index.ts',
+      helper: 'src/helper.ts'
+    },
+    output: [
+    { 
+      dir: "./dist",
+      format: 'es', 
+      sourcemap: false, 
+      plugins: [
+        flatDts({
+            entries: {
+              helper: {
+                as: "helper"
+              }
+            }
+        })
+      ]
+    }]
   }
 ];
